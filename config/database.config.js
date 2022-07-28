43518da2889116b09445fbd20d91a69a0b2c2575
ahhernandez <ahhernandez@indracompany.com>
2022-07-28 13:47:32 -0500

    Deploy to Heroku

---
+++ config/database.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    url: 'mongodb+srv://andres:BDroot999..@cluster0.imihn.mongodb.net/simpleapi?retryWrites=true&w=majority',
+    options: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    }
+   }
+   
